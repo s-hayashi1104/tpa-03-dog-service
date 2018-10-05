064c9c4215d699990c1533b410113bfbc64e750a
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-05 22:14:49 +0900

    sliceの値を修正 プロパティに値を入れる処理を修正

--- server/middleware.js
+++ server/middleware.js
@@ -3,9 +3,6 @@ const utils = require('./utils');
 const parsePathParametersMiddleware = (req, res, next) => {
   const params = utils.parsePathParameters(req.originalUrl, '/api/dog/:id');
   req.myParams = params;
-  // nextは何か？
-  // express middleware も理解しとこう：
-  // https://expressjs.com/ja/guide/using-middleware.html
   next();
 };
 
--- server/utils.js
+++ server/utils.js
@@ -1,25 +1,10 @@
-/**
- * parsePathParameters
- *
- * この関数は二つの文字列を与えられます。
- * path parameters を見極めてオブジェクトを返しましょう。
- * path parameters は pathWithParams 文字列の/の間にある:xxx がパラメターとなります。
- * 下記の例をご覧ください：
- *
- * 入力: '/posts/3/comments/4', '/posts/:postId/comments/:commentId'
- * 出力: {
- *   postId: 3,
- *   commentId: 4
- * }
- */
 const parsePathParameters = function(originalPath, pathWithParams) {
   const obj = {};
   const paramsArray = originalPath.split('/');
   pathWithParams.split('/').forEach((v, i) => {
     if (v[0] === ':') {
-      const key = v.slice(0);
+      const key = v.slice(1);
       const param = paramsArray[i];
-      Object.defineProperty(obj, key, {});
       obj[key] = param;
       console.log(obj);
     }
