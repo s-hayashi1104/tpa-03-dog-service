e7e8630d9e18a0a3558e44d0f809299c6b05df62
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-08 19:30:38 +0900

    if判定の書き方を修正

--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,7 @@ const dog = require('./dog');
 
 const fetchDogById = (req, res) => {
   const { id } = req.myParams;
-  if (!(typeof id === 'number')) {
+  if (typeof id !== 'number') {
     return res.status(400).json({});
   }
   const dogObj = dog.getDogById(id);
